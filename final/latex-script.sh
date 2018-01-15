55b64f0873d5e1452268d20ad219107e14ddae78
veikas@EZS-Labor <vagrawal@hs-esslingen.de>
2018-01-15 17:23:52 +0100

    change roman to latin numbers. ading latex script for automation
    Former-commit-id: 8e5edb9cd30ce6cc2226310c6fe2e6725910170b
    Former-commit-id: aa8b79b99dce32a74a9364f62845e66d5b8df827

--- final/latex-script.sh
+++ final/latex-script.sh
@@ -8,25 +8,34 @@ PART1=RT01-Real-Time-Environment.tex.pdf
 function convert_to_false()
 {
 	sed -i 's/overviewtrue/overviewfalse/' realtimesystems-beamer.tex
-	input="IIIIIIIIII"
-	for iterator in `seq 1 10`; do
-		sed -i 's/part${input:0:$iterator}true/part${input:0:$iterator}false/' realtimesystems-beamer.tex
-	done
+	for iterator in {1..10}; do #`seq 1 10`; do
+		match="part${iterator}true"
+		replace="part${iterator}false"
+		sed -i "s/${match}/${replace}/" realtimesystems-beamer.tex
+		#echo $command
+		#$command
+		done
 }
-
-convert_to_false
 
 function convert_overview()
 {
 	sed -i 's/overviewfalse/overviewtrue/' realtimesystems-beamer.tex
+	rm realtimesystems-beamer.pdf
 	xelatex -interaction=nonstopmode -synctex=1 realtimesystems-beamer.tex
 	mv realtimesystems-beamer.pdf convert/RT00-Introduction.tex.pdf
 }
 
 function convert_parts()
 {
-	sed -i 's/partIfalse/partItrue/' realtimesystems-beamer.tex
+	sed -i "s/part1false/part1true/" realtimesystems-beamer.tex
+	rm realtimesystems-beamer.pdf
 	xelatex -interaction=nonstopmode -synctex=1 realtimesystems-beamer.tex
-	mv realtimesystems-beamer.pdf convert/$PART1
+	mv realtimesystems-beamer.pdf final-pdfs/$PART1
 }
+
+convert_to_false
+read -p "Enter to continue"
+convert_overview
+read -p "Enter to continue"
+convert_parts
 
